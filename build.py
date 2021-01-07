d95d95c9bfbb832c031ca46b7143d1f2ed611cd8
stefan-beuchert <stefan.beuchert@gmx.de>
2021-01-07 20:49:07 +0100

    documentation added

--- build.py
+++ build.py
@@ -6,6 +6,7 @@ use_plugin("python.unittest")
 use_plugin("python.flake8")
 use_plugin("python.coverage")
 use_plugin("python.distutils")
+use_plugin("python.sphinx")
 
 
 name = "ContentBasedRecommenderSystem"
@@ -19,5 +20,9 @@ def set_properties(project):
     project.depends_on('pandas')
     project.depends_on('stop_words')
     project.depends_on('sklearn')
+
+    project.set_property('sphinx_config_path', 'docs/')
+    project.set_property('sphinx_source_dir', 'docs/source')
+    project.set_property('sphinx_output_dir', 'docs/_build')
 
     project.set_property("coverage_break_build", False)
--- src/main/python/processing/data_processing.py
+++ src/main/python/processing/data_processing.py
@@ -4,12 +4,26 @@ from in_out import save_data, connect_to_db
 
 
 def process_data(data, collection, *cleaning_parameters):
+    """
+    :param data: raw data as dataframe, that should be processed
+    :param collection: the db collection, where the result should be stored
+    :param cleaning_parameters: parameters for data cleaning
+    :return: dataframe with processed data
+    """
     processed_data = clean_data(data, *cleaning_parameters).reset_index()
     save_data(collection, processed_data)
     return processed_data
 
 
 def clean_data(df_raw, important_columns, target_feature, unwanted_characters, stop_word_language):
+    """
+    :param df_raw: raw data as datafram
+    :param important_columns: list of important columns
+    :param target_feature: the target feature
+    :param unwanted_characters: characters that should be removed
+    :param stop_word_language: language id (for example 'en' for english)
+    :return:
+    """
     # exclude unimportant columns
     df = df_raw[important_columns]
     # delete rows where at least one cell is nan
@@ -29,5 +43,10 @@ def clean_data(df_raw, important_columns, target_feature, unwanted_characters, s
 
 
 def remove_stopwords(text, stop_words):
+    """
+    :param text: string
+    :param stop_words: list of stop words
+    :return: string without stop words
+    """
     text_without_stopwords = ' '.join(word for word in text.split() if word not in stop_words)
     return text_without_stopwords
